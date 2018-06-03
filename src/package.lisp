ae0d8c4eb7ac2de1450c9693aa79a76c64c1001e
yanqirenshi <yanqirenshi@gmail.com>
2018-06-03 21:26:30 +0900

    add exports at *-internal

--- src/package.lisp
+++ src/package.lisp
@@ -1,12 +1,15 @@
 (in-package :cl-user)
 (defpackage :s-serialization
   (:use :cl)
-  (:export #:serializable-slots
-           #:serialize-xml
-           #:serialize-sexp
-           #:deserialize-xml
-           #:deserialize-sexp
-           #:make-serialization-state
+  (:export #:make-serialization-state
            #:reset-known-slots)
+  (:export #:serialize-xml
+           #:serialize-xml-internal
+           #:serializable-slots
+           #:deserialize-xml)
+  (:export #:serialize-sexp
+           #:serialize-sexp-internal
+           #:deserialize-sexp
+           #:deserialize-sexp-internal)
   (:documentation "XML and s-expression based serialization for Common Lisp and CLOS"))
 (in-package :s-serialization)
